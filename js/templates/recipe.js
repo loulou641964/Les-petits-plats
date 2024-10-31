5b1a5bc818e46fc336c75d746cc5667a5303daed
guy coste <guycoste86@gmail.com>
2024-10-31 22:59:20 +0100

    commit

--- js/templates/recipe.js
+++ js/templates/recipe.js
@@ -1,8 +1,22 @@
- function template (recipe){
-     return`
-    <article>
-        <img src="./assets/images/${recipe.image}" alt="${recipe.name}"> 
+function recipeTemplate(recipe) {
+    return `
+      <article class="recipe-card">
+        <img src="./assets/images/${recipe.image}" alt="${recipe.name}">
         <h3>${recipe.name}</h3>
-    </article>`
-}
-export default template
+        <p><strong>Temps de préparation :</strong> ${recipe.time} min</p>
+        
+        <ul class="ingredients-list">
+          <strong>Ingrédients :</strong>
+          ${recipe.ingredients.map(ingredient => `
+            <li>
+              ${ingredient.ingredient}: ${ingredient.quantity || ''} ${ingredient.unit || ''}
+            </li>`).join('')}
+        </ul>
+        
+        <p class="instructions"><strong>Instructions :</strong> ${recipe.instructions || 'Instructions non disponibles'}</p>
+      </article>
+    `;
+  }
+  
+  export default recipeTemplate;
+  
