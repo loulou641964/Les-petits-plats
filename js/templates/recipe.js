fe80f04c5b19da3e11097566a799da17718a1cc1
guy coste <guycoste86@gmail.com>
2024-11-01 21:01:20 +0100

    commit

--- js/templates/recipe.js
+++ js/templates/recipe.js
@@ -1,31 +1,27 @@
-function recipeTemplate(recipe) {
+function template(recipe) {
   return `
     <article class="recipe-card">
-      <img src="./assets/images/${recipe.image}" alt="${recipe.name}" class="recipe-image">
-      <div class="recipe-content">
-        <h3 class="recipe-title">${recipe.name}</h3>
-        
-        <!-- Section des ingrédients -->
-        <div class="recipe-section">
-          <h4 class="recipe-subtitle">INGRÉDIENTS</h4>
-          <ul class="ingredients-list">
-            ${recipe.ingredients.map(ingredient => `
-              <li>
-                <span class="ingredient-quantity">${ingredient.quantity || ''} ${ingredient.unit || ''}</span>
-                <span class="ingredient-name">${ingredient.ingredient}</span>
-              </li>`).join('')}
-          </ul>
-        </div>
-        
-        <!-- Section des instructions -->
-        <div class="recipe-section">
-          <h4 class="recipe-subtitle">RECETTE</h4>
-          <p class="recipe-instructions">${recipe.description || 'Instructions non disponibles'}</p>
-        </div>
-      </div>
+        <img src="./assets/images/${recipe.image}" alt="${recipe.name}" class="recipe-image">
+        <h2>${recipe.name}</h2>
+        <section class="recipe-details">
+            <h3>Recette</h3>
+            <p>${recipe.description}</p>
+            <h3>Ingrédients</h3>
+            <ul class="ingredients-list">
+                ${recipe.ingredients
+                  .map(
+                    (ingredient) => `
+                    <li>
+                      <span class="ingredient-name">${ingredient.ingredient}</span><br>
+                      <span class="ingredient-quantity">${ingredient.quantity || ''} ${ingredient.unit || ''}</span>
+                    </li>
+                  `
+                  )
+                  .join("")}
+            </ul>
+        </section>
     </article>
   `;
 }
-export default recipeTemplate;
-
+export default template;
 
