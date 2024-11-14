35d9469e020a2c5c6f3104ad7cdf2536cc8cf6f9
guy coste <guycoste86@gmail.com>
2024-11-14 15:16:32 +0100

    ajout

--- js/filters.js
+++ js/filters.js
@@ -5,12 +5,42 @@ function getUstensilsFromRecipes(recipes){
     ustensils = ustensils.concat(recipe.ustensils)    
     })
     const ustensilsUnique  = [...new Set(ustensils)];
+    console.log("Ustensiles récupérés :", ustensilsUnique);
     return ustensils
 }
-function fillListWithArray(list,data){
-    
+function getAppliancesFromRecipes(recipes) {
+    let appliances = [];
+    recipes.forEach(recipe => {
+        if (recipe.appliance) {
+            appliances.push(recipe.appliance);
+        }
+    });
+    const appliancesUnique = [...new Set(appliances)];
+    console.log("Appareils récupérés :", appliancesUnique);
+    return appliancesUnique;
 }
 
+function getIngredientsFromRecipes(recipes) {
+    let ingredients = [];
+    recipes.forEach(recipe => {
+        recipe.ingredients.forEach(item => {
+            ingredients.push(item.ingredient);
+        });
+    });
+    const ingredientsUnique = [...new Set(ingredients)];
+    console.log("Ingrédients récupérés :", ingredientsUnique);
+    return ingredientsUnique;
+}
+function fillListWithArray(list, data) {
+    // Vider la liste existante
+    list.innerHTML = '';
+    
+    // Parcourir le tableau de données et créer un élément de liste pour chaque entrée
+    data.forEach(item => {
+        const li = document.createElement('li');
+        li.textContent = item;
+        list.appendChild(li);
+    });
+}
 
-export {getUstensilsFromRecipes };
-
+export {getUstensilsFromRecipes,getAppliancesFromRecipes,getIngredientsFromRecipes,fillListWithArray };
