bec8725ec15061bcd424ffa3f9b90d812e098c42
guy coste <guycoste86@gmail.com>
2024-11-19 18:02:31 +0100

    à faire

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
-import { getAppliancesFromRecipes, getIngredientsFromRecipes, getUstensilsFromRecipes } from "./filters.js"
+import { getAppliancesFromRecipes, getIngredientsFromRecipes, getUstensilsFromRecipes,fillListWithArray } from "./filters.js"
 import recipes from "./../data/recipes.js"
 import recipeTemplate from "./templates/recipe.js"
 const container= document.querySelector(".container-recipes")
 recipes.forEach(recipe =>{ 
 container.innerHTML+= recipeTemplate(recipe);
 })
-getUstensilsFromRecipes(recipes)
-getAppliancesFromRecipes(recipes)
-getIngredientsFromRecipes(recipes)
+const Ustensiles = getUstensilsFromRecipes(recipes)
+const appliances = getAppliancesFromRecipes(recipes)
+const Ingredients = getIngredientsFromRecipes(recipes)
+
+const IngredientsListContainer = document.querySelector('#ingredients-liste')
+fillListWithArray(IngredientsListContainer, Ingredients )
+// ajouter un écouteur évenement sur imput des ingrédients.
+//   quand actif on veut faire aparaitre la liste des ingredients
+// quand on écrit une suite de lettre dans imput, on veut l'afficher dans la liste uniquement les ingredients avec ces lettres.
+// ex : si l'on écrit to on devrait avoir tomate qui s'affiche dans la liste. ex: lait de coco disparait de liste.
