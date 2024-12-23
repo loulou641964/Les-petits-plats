dd8a2cc919b49264df203a8f418e56c9d32bf3e8
guy coste <guycoste86@gmail.com>
2024-12-23 16:03:03 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// main.js
+
 // Importations nécessaires
 import { getAppliancesFromRecipes, getIngredientsFromRecipes, getUstensilsFromRecipes, fillListWithArray } from "./filters.js";
 import recipes from "./../data/recipes.js";
@@ -9,14 +11,14 @@ const selectedIngredients = [], selectedAppliances = [], selectedUstensils = [];
 
 // Mise à jour du compteur
 const updateRecipeCount = (count) => {
-  document.querySelector(".filter-text").textContent = `${count} recette${count > 1 ? 's' : ''}`;
+    document.querySelector(".filter-text").textContent = `${count} recette${count > 1 ? 's' : ''}`;
 };
 
 // Affichage des recettes
 const displayRecipes = (recipesToDisplay) => {
-  const container = document.querySelector(".container-recipes");
-  container.innerHTML = recipesToDisplay.map(recipeTemplate).join('');
-  updateRecipeCount(recipesToDisplay.length);
+    const container = document.querySelector(".container-recipes");
+    container.innerHTML = recipesToDisplay.map(recipeTemplate).join('');
+    updateRecipeCount(recipesToDisplay.length);
 };
 
 // Gestion des listes de filtres
@@ -33,84 +35,97 @@ fillListWithArray(appliancesListContainer, appliances);
 fillListWithArray(ustensilsListContainer, ustensils);
 
 const updateFilterLists = (filteredRecipes) => {
-  const updatedIngredients = getIngredientsFromRecipes(filteredRecipes)
-    .filter(ingredient => !selectedIngredients.includes(ingredient))
-    .sort((a, b) => a.localeCompare(b, 'fr', { sensitivity: 'base' }));
-  const updatedAppliances = getAppliancesFromRecipes(filteredRecipes)
-    .filter(appliance => !selectedAppliances.includes(appliance))
-    .sort((a, b) => a.localeCompare(b, 'fr', { sensitivity: 'base' }));
-  const updatedUstensils = getUstensilsFromRecipes(filteredRecipes)
-    .filter(ustensil => !selectedUstensils.includes(ustensil))
-    .sort((a, b) => a.localeCompare(b, 'fr', { sensitivity: 'base' }));
+    const updatedIngredients = getIngredientsFromRecipes(filteredRecipes)
+        .filter(ingredient => !selectedIngredients.includes(ingredient))
+        .sort((a, b) => a.localeCompare(b, 'fr', { sensitivity: 'base' }));
+    const updatedAppliances = getAppliancesFromRecipes(filteredRecipes)
+        .filter(appliance => !selectedAppliances.includes(appliance))
+        .sort((a, b) => a.localeCompare(b, 'fr', { sensitivity: 'base' }));
+    const updatedUstensils = getUstensilsFromRecipes(filteredRecipes)
+        .filter(ustensil => !selectedUstensils.includes(ustensil))
+        .sort((a, b) => a.localeCompare(b, 'fr', { sensitivity: 'base' }));
 
-  fillListWithArray(ingredientsListContainer, updatedIngredients);
-  fillListWithArray(appliancesListContainer, updatedAppliances);
-  fillListWithArray(ustensilsListContainer, updatedUstensils);
+    fillListWithArray(ingredientsListContainer, updatedIngredients);
+    fillListWithArray(appliancesListContainer, updatedAppliances);
+    fillListWithArray(ustensilsListContainer, updatedUstensils);
 };
 
 const filterRecipes = () => {
-  const searchInput = document.querySelector(".search-input");
-  const searchTerm = searchInput.value.trim();
-  let filteredRecipes = searchTerm.length >= 3 ? mainSearch(searchTerm) : recipes;
+    const searchInput = document.querySelector(".search-input");
+    const searchTerm = searchInput.value.trim();
 
-  filteredRecipes = filteredRecipes.filter(recipe => {
-    const hasIngredients = selectedIngredients.every(ingredient =>
-      recipe.ingredients.some(item => item.ingredient.toLowerCase() === ingredient.toLowerCase())
-    );
-    const hasAppliances = selectedAppliances.every(appliance =>
-      recipe.appliance.toLowerCase() === appliance.toLowerCase()
-    );
-    const hasUstensils = selectedUstensils.every(ustensil =>
-      recipe.ustensils.some(item => item.toLowerCase() === ustensil.toLowerCase())
-    );
-    return hasIngredients && hasAppliances && hasUstensils;
-  });
+    let filteredRecipes = searchTerm.length >= 3 ? mainSearch(searchTerm) : recipes;
 
-  displayRecipes(filteredRecipes);
-  updateRecipeCount(filteredRecipes.length);
-  updateFilterLists(filteredRecipes);
+    filteredRecipes = filteredRecipes.filter(recipe => {
+        const hasIngredients = selectedIngredients.every(ingredient =>
+            recipe.ingredients.some(item =>
+                item.ingredient.toLowerCase() === ingredient.toLowerCase()
+            )
+        );
+        
+        const hasAppliances = selectedAppliances.every(appliance =>
+            recipe.appliance.toLowerCase() === appliance.toLowerCase()
+        );
+        
+        const hasUstensils = selectedUstensils.every(ustensil =>
+            recipe.ustensils.some(item =>
+                item.toLowerCase() === ustensil.toLowerCase()
+            )
+        );
+
+        return hasIngredients && hasAppliances && hasUstensils;
+    });
+
+    displayRecipes(filteredRecipes);
+    updateRecipeCount(filteredRecipes.length);
+    updateFilterLists(filteredRecipes);
 };
 
+  
+
 const handleChevronToggle = (showChevron, hideChevron, listContainer, input, items) => {
-    const searchInput = document.createElement('input');
-    searchInput.type = 'text';
-    searchInput.placeholder = 'Rechercher...';
-    searchInput.classList.add('mini-search');
-  
-    const searchIcon = document.createElement('i');
-    searchIcon.classList.add('fa', 'fa-search', 'mini-search-icon');
-  
-    const searchWrapper = document.createElement('div');
-    searchWrapper.classList.add('mini-search-wrapper');
-    searchWrapper.appendChild(searchInput);
-    searchWrapper.appendChild(searchIcon);
-  
     showChevron.addEventListener("click", () => {
       listContainer.style.display = "block";
       showChevron.style.display = "none";
       hideChevron.style.display = "inline-block";
-      showChevron.closest('.filter-item').classList.add('open');
-      listContainer.insertBefore(searchWrapper, listContainer.firstChild);
-      searchInput.focus();
+      showChevron.closest('.filter-item').classList.add('open'); // Ajout de cette ligne
     });
   
     hideChevron.addEventListener("click", () => {
       listContainer.style.display = "none";
       hideChevron.style.display = "none";
       showChevron.style.display = "inline-block";
-      hideChevron.closest('.filter-item').classList.remove('open');
-      if (searchWrapper.parentNode) {
-        searchWrapper.remove();
-      }
+      hideChevron.closest('.filter-item').classList.remove('open'); // Ajout de cette ligne
     });
   
-    searchInput.addEventListener("input", () => {
-      const searchTerm = searchInput.value.toLowerCase().trim();
-      const filteredItems = items.filter(item => item.toLowerCase().includes(searchTerm));
-      fillListWithArray(listContainer, filteredItems);
+    if (input) {
+      input.addEventListener("focus", () => {
+        listContainer.style.display = "block";
+        showChevron.style.display = "none";
+        hideChevron.style.display = "inline-block";
+        input.closest('.filter-item').classList.add('open'); // Ajout de cette ligne
+      });
+  
+      input.addEventListener("blur", () => {
+        setTimeout(() => {
+          listContainer.style.display = "none";
+          hideChevron.style.display = "none";
+          showChevron.style.display = "inline-block";
+          input.closest('.filter-item').classList.remove('open'); // Ajout de cette ligne
+        }, 200);
     });
+      
+    
   
   
+        input.addEventListener("input", () => {
+            const searchTerm = input.value.toLowerCase().trim();
+            const filteredItems = items.filter(item => 
+                item.toLowerCase().includes(searchTerm)
+            );
+            fillListWithArray(listContainer, filteredItems);
+        });
+    }
 };
 
 const showIngredientsChevron = document.querySelector("#show-ingredients");
@@ -131,43 +146,48 @@ handleChevronToggle(showUstensilsChevron, hideUstensilsChevron, ustensilsListCon
 const selectedTagContainer = document.querySelector("#selected-tags");
 
 const updateTags = () => {
-  selectedTagContainer.innerHTML = '';
-  const createTag = (tag, selectedArray, container) => {
-    const span = document.createElement("span");
-    span.textContent = tag;
-    span.classList.add("tag");
-    const closeButton = document.createElement("button");
-    closeButton.textContent = "X";
-    closeButton.classList.add("close-tag");
-    closeButton.addEventListener("click", () => {
-      selectedArray.splice(selectedArray.indexOf(tag), 1);
-      updateTags();
-      filterRecipes();
-    });
-    span.appendChild(closeButton);
-    container.appendChild(span);
-  };
+    selectedTagContainer.innerHTML = '';
+    
+    const createTag = (tag, selectedArray, container) => {
+        const span = document.createElement("span");
+        span.textContent = tag;
+        span.classList.add("tag");
 
-  selectedIngredients.forEach(tag => createTag(tag, selectedIngredients, selectedTagContainer));
-  selectedAppliances.forEach(tag => createTag(tag, selectedAppliances, selectedTagContainer));
-  selectedUstensils.forEach(tag => createTag(tag, selectedUstensils, selectedTagContainer));
+        const closeButton = document.createElement("button");
+        closeButton.textContent = "X";
+        closeButton.classList.add("close-tag");
+
+        closeButton.addEventListener("click", () => {
+            selectedArray.splice(selectedArray.indexOf(tag), 1);
+            updateTags();
+            filterRecipes();
+        });
+
+        span.appendChild(closeButton);
+        container.appendChild(span);
+    };
+
+    selectedIngredients.forEach(tag => createTag(tag, selectedIngredients, selectedTagContainer));
+    selectedAppliances.forEach(tag => createTag(tag, selectedAppliances, selectedTagContainer));
+    selectedUstensils.forEach(tag => createTag(tag, selectedUstensils, selectedTagContainer));
 };
 
 const handleSelection = (listContainer, selectedArray, showChevron, hideChevron) => {
-  listContainer.addEventListener("click", e => {
-    if (e.target.tagName === "LI") {
-      const value = e.target.textContent.trim();
-      if (!selectedArray.includes(value)) {
-        selectedArray.push(value);
-        updateTags();
-        filterRecipes();
-        listContainer.style.display = "none";
-        hideChevron.style.display = "none";
-        showChevron.style.display = "inline-block";
-      }
-    }
-  });
+    listContainer.addEventListener("click", e => {
+        if (e.target.tagName === "LI") {
+            const value = e.target.textContent.trim();
+            if (!selectedArray.includes(value)) {
+                selectedArray.push(value);
+                updateTags();
+                filterRecipes();
+                listContainer.style.display = "none";
+                hideChevron.style.display = "none";
+                showChevron.style.display = "inline-block";
+            }
+        }
+    });
 };
+
 
 handleSelection(ingredientsListContainer, selectedIngredients, showIngredientsChevron, hideIngredientsChevron);
 handleSelection(appliancesListContainer, selectedAppliances, showAppliancesChevron, hideAppliancesChevron);
